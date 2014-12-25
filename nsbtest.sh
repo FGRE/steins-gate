bb0061c1202054eb72c7dfcfa1a5d597bb2511a5
Mislav Blažević <krofnica996@gmail.com>
2014-12-25 11:54:06 +0100

    add nsbtest.sh: A script to compile, pack and run nss using STEINSGATE.exe

---
+++ nsbtest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+../nptools/nsbcompile2 $1
+../nptools/npaextract nss.npa SteinsGate
+mv ${1::-3}"nsb" nss/0_boot.nsb
+mv ${1::-3}"map" nss/0_boot.map
+mv nss.npa nss.npa.bak
+../nptools/npapack nss
+rm -fr nss
+LANG=ja_JP wine STEINSGATE.exe
+mv nss.npa.bak nss.npa
